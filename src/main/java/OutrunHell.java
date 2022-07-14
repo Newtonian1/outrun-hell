727d2aa670ff132ef5d1f63fb7d62217941c289d
Newtonian1 <ross3jh@gmail.com>
2022-07-13 22:38:57 -0400

    Added wait function and created Test class to test event flow

--- src/main/java/OutrunHell.java
+++ src/main/java/OutrunHell.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 
 public class OutrunHell {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Please enter a word to have it repeated back to you:");
-        String input1 = userInput.nextLine();
-        System.out.println(input1);
-        System.out.println(input1);
+        System.out.println("...");
+        wait(2);
+        System.out.println("Hey, can you hear me?");
+        wait(2);
+        System.out.println("Nice to meet you, the name's Satan!");
+        wait(2);
+
+    }
+    public static void wait(int s) {
+        try {
+            TimeUnit.SECONDS.sleep(s);
+        }
+        catch(InterruptedException ignored) {
+        }
     }
 }
